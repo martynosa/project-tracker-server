084da266a0e6c694de18216a57d55d20230f5f27
martiN <78382341+martynosa@users.noreply.github.com>
2022-01-20 16:40:56 +0200

    removed unused search service

--- src/services/services.js
+++ src/services/services.js
@@ -8,24 +8,11 @@ const deleteItem = (idToDelete) => itemModel.findByIdAndDelete(idToDelete)
 
 const updateItem = (idToUpdate, newItem) => itemModel.findByIdAndUpdate(idToUpdate, newItem, { runValidators: true })
 
-//! shows duplicates
-const search = async (searchCriteria) => {
-    let results = await getAllGames()
-    if (searchCriteria) {
-        const byTitle = results.filter(g => g.title.toLowerCase().includes(searchCriteria.toLowerCase()))
-        const byGanre = results.filter(g => g.ganre.toLowerCase().includes(searchCriteria.toLowerCase()))
-        const byYear = results.filter(g => g.releaseYear == Number(searchCriteria))
-        results = [...byTitle, ...byGanre, ...byYear]
-    };
-    return results
-};
-
 const services = {
     getSingleItem,
     createItem,
     deleteItem,
     updateItem,
-    search
 }
 
 module.exports = services
