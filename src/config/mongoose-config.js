55f2a2644bf525c675265b285daba3953842648e
martiN <78382341+martynosa@users.noreply.github.com>
2022-02-08 00:01:22 +0200

    mongodb changed to another one

--- src/config/mongoose-config.js
+++ src/config/mongoose-config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const initMongoose = () => mongoose.connect('mongodb+srv://martynosa:projecttracker@cluster0.goiey.mongodb.net/test')
+const initMongoose = () => mongoose.connect('mongodb+srv://martynosa:projecttracker@cluster0.goiey.mongodb.net/project-tracker')
 
 module.exports = initMongoose
