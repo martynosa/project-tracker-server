b389b12c6fe245b43f1b9c4e553c5c6731f76806
martiN <78382341+martynosa@users.noreply.github.com>
2022-05-02 21:46:23 +0300

    added demo project after user registration

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,14 +1,50 @@
 const express = require('express')
 const authServices = require('../services/authServices')
+const services = require('../services/services')
 
 const router = express.Router()
 
 const registerUser = async (req, res) => {
     const userToRegister = req.body
+    const defaultProject = {
+        name: 'Project Mars (Demo)',
+        keywords: ['MARS', 'LANDING', 'HUMANS'],
+        description: 'The first crewed mission to Mars.',
+        status: 'new',
+        tasks: [
+            {
+                "task": "Rover exploration.",
+                "key": "1",
+                "isCompleted": true
+            },
+            {
+                "task": "Send base components",
+                "key": "2",
+                "isCompleted": false
+            },
+            {
+                "task": "Send crew to Mars.",
+                "key": "3",
+                "isCompleted": false
+            },
+            {
+                "task": "Build base.",
+                "key": "4",
+                "isCompleted": false
+            }, {
+                "task": "Return crew from Mars.",
+                "key": "5",
+                "isCompleted": false
+            }
+        ]
+    }
+
     try {
         await authServices.registerUser(userToRegister)
         const loggedUser = await authServices.logUser(userToRegister)
         const token = await authServices.createToken(loggedUser)
+        const createdItem = await services.createItem({ ...defaultProject, ownerId: loggedUser._id })
+        await authServices.addToCreatedItems(loggedUser._id, createdItem._id)
         res.status(200).json({
             id: loggedUser._id,
             username: loggedUser.username,
