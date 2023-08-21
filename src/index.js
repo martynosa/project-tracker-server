70ebab8a24c7bfdd24640d50770cf0d1955e2298
martiN <78382341+martynosa@users.noreply.github.com>
2023-08-21 14:21:51 +0300

    CORS policy updated

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
-const initMongoose = require('./config/mongooseConfig');
-const middlewares = require('./services/middlewares');
-const router = require('./router');
-const errorHandler = require('./services/errors/errorHandler');
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const initMongoose = require("./config/mongooseConfig");
+const middlewares = require("./services/middlewares");
+const router = require("./router");
+const errorHandler = require("./services/errors/errorHandler");
 
 const PORT = process.env.PORT || 5000;
 
-dotenv.config({ path: 'src/config.env' });
+dotenv.config({ path: "src/config.env" });
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, "./public")));
 //CORS
 app.use((req, res, next) => {
-  process.env.NODE_ENV === 'production'
-    ? res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://martynosa-project-tracker.netlify.app'
-      )
-    : res.setHeader('Access-Control-Allow-Origin', '*');
+  process.env.NODE_ENV === "production"
+    ? res.setHeader("Access-Control-Allow-Origin", `${process.env.HOST}`)
+    : res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, token');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, token");
   next();
 });
 app.use(middlewares.auth);
@@ -38,4 +35,4 @@ initMongoose()
       console.log(`env = ${process.env.NODE_ENV} and listening on ${PORT}...`)
     )
   )
-  .catch((error) => console.log('mongoose error:' + error));
+  .catch((error) => console.log("mongoose error:" + error));
